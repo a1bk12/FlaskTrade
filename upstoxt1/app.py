b1e53164db2a79e1157f1950e64e072392b6ced7
a1bk12 <140173934+a1bk12@users.noreply.github.com>
2024-08-12 20:43:35 +0530

    commit 8
    commit 8

--- upstoxt1/app.py
+++ upstoxt1/app.py
@@ -1,15 +1,10 @@
-from flask import Flask, request
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route('/')
 def home():
-    # Extract the 'code' parameter from the query string
-    auth_code = request.args.get('code')
-    if auth_code:
-        return f"Authorization Code: {auth_code}", 200
-    else:
-        return "No authorization code found in the request.", 400
+    return "Hello, World!"
 
 if __name__ == "__main__":
     app.run()
